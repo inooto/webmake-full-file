f0011000b248127a67940d1332ebedddab5a566e
inoue otoki <148949499+inooto@users.noreply.github.com>
2024-11-28 22:39:19 +0900

    Delete sawarabi directory

--- sawarabi/wp-content/themes/swell/build/gutenberg/post_editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '17a18802f8c76907751b');
