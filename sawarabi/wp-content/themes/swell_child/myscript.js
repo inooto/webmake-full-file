36d00d8de68a17c72864b7bc138fe66a7223bb9c
inooto <inooto0421@icloud.com>
2024-11-28 22:00:51 +0900

    add: sawarabi

---
+++ sawarabi/wp-content/themes/swell_child/myscript.js
@@ -0,0 +1,49 @@
+/*-----文字パラパラ-----*/
+function EachTextAnimeControl(){
+    $('.eachTextAnime').each(function(){
+        var elemPos = $(this).offset().top - 50;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight){
+            $(this).addClass("appeartext");
+        }else{
+            $(this).removeClass("appeartext");
+        }
+    });
+}
+
+$(window).on('load', function(){
+    var element = $(".eachTextAnime");
+    element.each(function(){
+        var text = $(this).text();
+        var textbox = "";
+        text.split('').forEach(function(t, i){
+            if (t !== ""){
+                if (i < 10){
+                    textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>'
+                }else{
+                    var n = i/10;
+                    textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>'
+                }
+            }else{
+                textbox += t;
+            }
+        });
+        $(this).html(textbox);
+    });
+    EachTextAnimeControl();
+});
+
+/*-----記事スライダー-----*/
+$('.slider').slick({
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    centerMode: true,
+    variableWidth: true,
+    dots: true,
+});
